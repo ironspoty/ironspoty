e7d44e14795db67888dbbd23691f8fea048f3d58
Ignacio Garcia Barrero <igarciabarrero@outlook.com>
2020-02-26 18:40:51 +0100

    Graph representation

---
+++ public/javascripts/chart.js
@@ -0,0 +1,49 @@
+axios.get('/userData').then(response => {
+
+  console.log("Access of general array", response.data.data.items);
+  console.log("Primer Track", response.data.data.items[0].track.album.artists[0].id);
+
+  let arrayTracks = response.data.items;
+  console.log("Este es un array de tracks???", arrayTracks)
+  //arrayTracks.forEach((e) => console.log(e))
+
+
+  const parameters = [0, 1, 2, 3, 4, 5, 6, 7]
+  const ctx = document.getElementById('myChart').getContext('2d');
+  const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [0, 1, 2, 3, 4, 5, 6, 7],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
+})
