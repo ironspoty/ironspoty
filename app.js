020c48bcb9c01af4bdcdfcb6d25036389ffd6f93
Carlos Mateo <cmc2k5@gmail.com>
2020-02-28 05:52:19 +0100

    Login and SignUp styles + feather icon dependency added

--- app.js
+++ app.js
@@ -87,7 +87,6 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Express View engine setup
-
 app.use(require('node-sass-middleware')({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
