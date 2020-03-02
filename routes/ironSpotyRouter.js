6d54437d22e9fdd3ac3547e21026bafc88006b16
Carlos Mateo <cmc2k5@gmail.com>
2020-03-02 16:39:53 +0100

    User preferences for friends

--- routes/ironSpotyRouter.js
+++ routes/ironSpotyRouter.js
@@ -31,7 +31,6 @@ const SCOPES = process.env.SCOPES;
 
 let spotyAccessToken = '';
 
-
 // SIGN UP
 ironSpotyRouter.get("/signup", isLoggedOut(), (req, res, next) => {
     res.render("app/signup");
@@ -44,7 +43,6 @@ ironSpotyRouter.post("/signup", isLoggedOut(), (req, res, next) => {
     if (username === "" || password === "") {
         req.flash("error", "All fields are required");
         return res.redirect("/signup");
-
     }
 
     User.findOne({ username })
@@ -101,6 +99,10 @@ ironSpotyRouter.get("/logout", ensureLogin.ensureLoggedIn(), (req, res, next) =>
 // SPOTIFY
 ironSpotyRouter.get("/spotify", ensureLogin.ensureLoggedIn(), (req, res) => {
 
+    if (!req.user.userSpotifyData) {
+        res.redirect('/profile');
+    }
+
     const scopes = SCOPES;
     const my_client_id = CLIENT_ID;
     const redirect_uri = REDIRECT_URI;
@@ -120,29 +122,31 @@ ironSpotyRouter.get("/callback", ensureLogin.ensureLoggedIn(), async (req, res) 
         res.redirect('/auth-error');
     }
 
-    const spotifyToken = await axios({
-        method: 'post',
-        url: 'https://accounts.spotify.com/api/token',
-        headers: {
-            'Authorization': 'Basic ' + new Buffer(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64'),
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        data: querystring.stringify({ grant_type: GRANT_TYPE, code: req.query.code, redirect_uri: REDIRECT_URI }),
-        responseType: 'json'
-    });
+    if (req.user.userSpotifyData) {
+        const spotifyToken = await axios({
+            method: 'post',
+            url: 'https://accounts.spotify.com/api/token',
+            headers: {
+                'Authorization': 'Basic ' + new Buffer(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64'),
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            data: querystring.stringify({ grant_type: GRANT_TYPE, code: req.query.code, redirect_uri: REDIRECT_URI }),
+            responseType: 'json'
+        });
 
-    spotyAccessToken = spotifyToken.data.access_token;
+        spotyAccessToken = spotifyToken.data.access_token;
 
-    const spotifyUserData = await axios({
-        method: 'get',
-        url: `https://api.spotify.com/v1/me`,
-        headers: { 'Authorization': `Bearer ${spotyAccessToken}` }
-    });
+        const spotifyUserData = await axios({
+            method: 'get',
+            url: `https://api.spotify.com/v1/me`,
+            headers: { 'Authorization': `Bearer ${spotyAccessToken}` }
+        });
 
-    await User.findByIdAndUpdate(req.user._id, {
-        userSpotifyData: spotifyUserData.data,
-        avatar: spotifyUserData.data.images[0].url
-    });
+        await User.findByIdAndUpdate(req.user._id, {
+            userSpotifyData: spotifyUserData.data,
+            avatar: spotifyUserData.data.images[0].url
+        });
+    }
 
     res.redirect('/profile');
 });
@@ -158,22 +162,6 @@ ironSpotyRouter.get('/', ensureLogin.ensureLoggedIn(), (req, res, next) => {
         res.redirect('/profile');
     else
         res.redirect('/login');
-});
-
-ironSpotyRouter.get("/friends", ensureLogin.ensureLoggedIn(), async (req, res, next) => {
-    const user = await User
-        .find({ '_id': req.user._id })
-        .populate('friends');
-
-    let friends = user[0].friends.map(friend => {
-        friend.currentlyPlaying.artists = typeof friend.currentlyPlaying.artists === 'object' ?
-            friend.currentlyPlaying.artists.map(artist => artist.name).join(', ') :
-            friend.currentlyPlaying.artists
-        friend.recentlyPlayed = friend.recentlyPlayed.slice(0, 3);
-        return friend;
-    })
-
-    res.render('app/friends', { friends })
 });
 
 ironSpotyRouter.get("/map", ensureLogin.ensureLoggedIn(), async (req, res, next) => {
@@ -194,66 +182,89 @@ ironSpotyRouter.get("/map", ensureLogin.ensureLoggedIn(), async (req, res, next)
     res.render("app/map", { usersDataJSON: JSON.stringify(usersData), usersData: usersData });
 });
 
+ironSpotyRouter.get("/friends", ensureLogin.ensureLoggedIn(), async (req, res, next) => {
+    const user = await User
+        .find({ '_id': req.user._id })
+        .populate('friends');
+
+    let friends = user[0].friends.map(friend => {
+        friend.currentlyPlaying.artists = typeof friend.currentlyPlaying.artists === 'object' ?
+            friend.currentlyPlaying.artists.map(artist => artist.name).join(', ') :
+            friend.currentlyPlaying.artists
+        friend.recentlyPlayed = friend.recentlyPlayed.slice(0, 3);
+        return friend;
+    })
+
+    res.render('app/friends', { friends })
+});
+
 ironSpotyRouter.get("/profile", ensureLogin.ensureLoggedIn(), async (req, res, next) => {
 
-    if (!spotyAccessToken) {
-        res.redirect('/logout');
-    }
-
-    const recentlyPlayed = await axios({
-        url: `https://api.spotify.com/v1/me/player/recently-played?limit=10`,
-        headers: { 'Authorization': `Bearer ${spotyAccessToken}` },
-        transformResponse: [(data) => {
-            let transformedData = JSON.parse(data);
-
-            return transformedData.items.map(item => {
-                return {
-                    "name": item.track.name,
-                    "popularity": item.track.popularity,
-                    "artists": item.track.artists.map(artist => artist.name).join(', '),
-                    "played_at": item.played_at,
-                    "duration_ms": item.track.duration_ms,
-                    "spotifyUrl": item.track.external_urls.spotify,
-                    "spotifyId": item.track.id
-                }
-            })
-        }],
-    })
-
-    const currentlyPlaying = await axios({
-        method: 'get',
-        url: `https://api.spotify.com/v1/me/player/currently-playing`,
-        headers: { 'Authorization': `Bearer ${spotyAccessToken}` },
-        transformResponse: [(data) => {
-            if (data) {
-
+    if (req.user.userSpotifyData) {
+        const recentlyPlayed = await axios({
+            url: `https://api.spotify.com/v1/me/player/recently-played?limit=10`,
+            headers: { 'Authorization': `Bearer ${spotyAccessToken}` },
+            transformResponse: [(data) => {
+                console.log(data);
                 let transformedData = JSON.parse(data);
 
-                return {
-                    "name": transformedData.item.name,
-                    "popularity": transformedData.item.popularity,
-                    "artists": transformedData.item.artists.map(artist => artist.name).join(', '),
-                    "played_at": transformedData.item.played_at,
-                    "duration_ms": transformedData.item.duration_ms,
-                    "spotifyUrl": transformedData.item.external_urls.spotify,
-                    "spotifyId": transformedData.item.id
+                return transformedData.items.map(item => {
+                    return {
+                        "name": item.track.name,
+                        "popularity": item.track.popularity,
+                        "artists": item.track.artists.map(artist => artist.name).join(', '),
+                        "played_at": item.played_at,
+                        "duration_ms": item.track.duration_ms,
+                        "spotifyUrl": item.track.external_urls.spotify,
+                        "spotifyId": item.track.id
+                    }
+                })
+            }],
+        })
+
+        const currentlyPlaying = await axios({
+            method: 'get',
+            url: `https://api.spotify.com/v1/me/player/currently-playing`,
+            headers: { 'Authorization': `Bearer ${spotyAccessToken}` },
+            transformResponse: [(data) => {
+                if (data) {
+
+                    let transformedData = JSON.parse(data);
+
+                    return {
+                        "name": transformedData.item.name,
+                        "popularity": transformedData.item.popularity,
+                        "artists": transformedData.item.artists.map(artist => artist.name).join(', '),
+                        "played_at": transformedData.item.played_at,
+                        "duration_ms": transformedData.item.duration_ms,
+                        "spotifyUrl": transformedData.item.external_urls.spotify,
+                        "spotifyId": transformedData.item.id
+                    }
                 }
-            }
-        }]
-    })
+            }]
+        })
 
-    const updatedUser = await User.findByIdAndUpdate(req.user._id, {
-        recentlyPlayed: recentlyPlayed.data,
-        currentlyPlaying: currentlyPlaying.data
-    });
+        var updatedUser = await User.findByIdAndUpdate(req.user._id, {
+            recentlyPlayed: recentlyPlayed.data,
+            currentlyPlaying: currentlyPlaying.data
+        });
+    }
 
-    let userData = updatedUser;
+    let userData = updatedUser || req.user;
     userData.cantBeFriended = true;
     userData.notMe = userData.id !== req.user._id;
     userData.posts = await Post.find({ 'author': req.user._id }).sort({ 'createdAt': -1 });
 
     res.render("app/profile", { userData });
 
+});
+
+ironSpotyRouter.get("/preferences", ensureLogin.ensureLoggedIn(), async (req, res, next) => {
+    res.render('app/preferences', { user: req.user })
+});
+
+ironSpotyRouter.get("/preferences-user-info", ensureLogin.ensureLoggedIn(), async (req, res, next) => {
+    res.render('app/preferences-user-info')
 });
 
 ironSpotyRouter.post("/new-post", ensureLogin.ensureLoggedIn(), async (req, res, next) => {
@@ -268,6 +279,39 @@ ironSpotyRouter.post("/new-post", ensureLogin.ensureLoggedIn(), async (req, res,
     });
 
     res.redirect("/profile");
+});
+
+ironSpotyRouter.post("/update-user-info", ensureLogin.ensureLoggedIn(), async (req, res, next) => {
+
+    const { password, password_confirm } = req.body;
+
+    if (password === password_confirm) {
+        req.flash("error", "Password and Confirm password must match");
+        return res.redirect("/preferences-user-info");
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    await User.findByIdAndUpdate(_id, {
+        password: hashPass
+    });
+
+    res.redirect("/update-user-info");
+});
+
+ironSpotyRouter.post("/update-user-personal", ensureLogin.ensureLoggedIn(), async (req, res, next) => {
+
+    const { _id } = req.user;
+    const { name, lastname, email } = req.body;
+
+    await User.findByIdAndUpdate(_id, {
+        name,
+        lastname,
+        email
+    });
+
+    res.redirect("/preferences");
 });
 
 
